11cfa2e470bc2a25e3da62bca59d4fd46dac3571
vishu <108395193+Vishu-phogat@users.noreply.github.com>
2024-06-28 01:37:11 +0530

    Update InputArea.jsx

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -2,6 +2,15 @@ import React, { useState } from "react";
 
 function InputArea(props) {
   const [inputText, setInputText] = useState("");
+  const [isMousedOver, setMouseOver] = useState(false);
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
 
   function handleChange(event) {
     const newValue = event.target.value;
@@ -12,10 +21,13 @@ function InputArea(props) {
     <div className="form">
       <input onChange={handleChange} type="text" value={inputText} />
       <button
+        style={{ backgroundColor: isMousedOver ? "red" : "blue" }}
         onClick={() => {
           props.OnAdd(inputText);
           setInputText("");
         }}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
       >
         <span>Add</span>
       </button>
